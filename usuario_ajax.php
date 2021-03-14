399445cc7b4321a7ca18dd46eb21413a2c9e5189
Jonathan Monteiro <jonathanmonteirodev@gmail.com>
2021-03-14 17:38:20 -0300

    Usuarios ajax

---
+++ usuario_ajax.php
@@ -0,0 +1,29 @@
+<?php
+$db_handle = pg_connect("host=ec2-54-164-22-242.compute-1.amazonaws.com dbname=dc5ssns9cdsk52 user=pzwruillafjvix password=f4b462b358e278660c11bfd243ead82ad574b2ad7fec4244a2ea52e8bfde059c");
+
+$data = new DateTime();
+$agora = ($data->sub(new DateInterval('PT3H')))->format('d/m/Y H:i:s');
+
+if (($_GET['acao']) == "inserir_single") {
+    $nome = trim($_POST['nome']);
+    $idade = trim($_POST['idade']);
+
+    $query = pg_query("INSERT INTO Usuario(`nome`,`data`,`idade`,`multiplayer`) VALUES ('$nome', '$agora', '$idade', '0') RETURNING Currval('Usuario_id_seq')");
+    $fch = pg_fetch_row($query);
+    echo $fch[0];
+}else if (($_GET['acao']) == "inserir_multi") {
+    $nome = trim($_POST['nome']);
+    $idade = trim($_POST['idade']);
+    $nome2 = trim($_POST['nome2']);
+    $idade2 = trim($_POST['idade2']);
+
+    $query = pg_query("INSERT INTO Usuario(`nome`,`data`,`idade`,`multiplayer`) VALUES ('$nome', '$agora', '$idade', '1') RETURNING Currval('Usuario_id_seq')";
+    $fch = pg_fetch_row($query);
+    echo $fch[0];
+
+    $query = pg_query("INSERT INTO Usuario(`nome`,`data`,`idade`,`multiplayer`) VALUES ('$nome2', '$agora', '$idade2', '1') RETURNING Currval('Usuario_id_seq')";
+    echo ',';
+    $fch = pg_fetch_row($query);
+    echo $fch[0];
+}
+?>
