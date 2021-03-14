90c6f09479fd70b4e50c36cd0afc6dae86a6f72b
gestusjogo <40338240+gestusjogo@users.noreply.github.com>
2021-03-13 23:19:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php 
-echo 'e ai';
+// abre o arquivo colocando o ponteiro de escrita no final
+$arquivo = fopen('meuarquivo.txt','r+');
+if ($arquivo) {
+	if (!fwrite($arquivo, 'Novo conteúdo')) die('Não foi possível atualizar o arquivo.');
+	echo 'Arquivo atualizado com sucesso';
+	fclose($arquivo);
+}
 ?>
