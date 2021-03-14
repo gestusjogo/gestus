6a19c49e36120cbb2095e742bae4858c6cd03822
Jonathan Monteiro <jonathanmonteirodev@gmail.com>
2021-03-14 17:49:58 -0300

    Vai da certo

--- usuario_ajax.php
+++ usuario_ajax.php
@@ -8,7 +8,7 @@ if (($_GET['acao']) == "inserir_single") {
     $nome = trim($_GET['nome']);
     $idade = trim($_GET['idade']);
 
-    $query = "INSERT INTO Usuario(`nome`,`data`,`idade`,`multiplayer`) VALUES ('$nome', '$agora', '$idade', '0')";
+    $query = "INSERT INTO Usuario(nome,data,idade,multiplayer) VALUES ('$nome', '$agora', '$idade', '0')";
     $result = pg_query($db_handle,$query);
     echo $result;
 }
