c9030556b252dc72b6c3d55d28b5dcb411a6c008
Jonathan Monteiro <jonathanmonteirodev@gmail.com>
2021-03-14 21:46:40 -0300

    Mudanças no gerador

--- certificado/gerador.php
+++ certificado/gerador.php
@@ -4,14 +4,7 @@ date_default_timezone_set( 'America/Sao_Paulo' );
 require('fpdf/alphapdf.php');
 require('PHPMailer/class.phpmailer.php');
 
-
-// --------- Variáveis do Formulário ----- //
-// $email    = $_POST['email'];
-// $nome     = utf8_decode($_POST['nome']);
-// $cpf      = $_POST['cpf'];
-$email    = 'jonathanmonteirodev@gmail.com';
-$nome     =  $_GET['nome'];
-$cpf      = '05109064350';
+$nome     =  utf8_decode($_GET['nome']);
 $idade = $_GET['idade'];
 
 // --------- Variáveis que podem vir de um banco de dados por exemplo ----- //
@@ -64,23 +57,6 @@ $pdf->SetXY(32,172); //Parte chata onde tem que ficar ajustando a posição X e 
 $pdf->MultiCell(165, 10, $texto3, '', 'L', 0); // Tamanho width e height e posição
 
 $pdfdoc = $pdf->Output('', 'S');
-
-
-
-// ******** Agora vai enviar o e-mail pro usuário contendo o anexo
-// ******** e também mostrar na tela para caso o e-mail não chegar
-/*
-$subject = 'Seu Certificado do Workshop';
-$messageBody = "Olá $nome<br><br>É com grande prazer que entregamos o seu certificado.<br>Ele está em anexo nesse e-mail.<br><br>Atenciosamente,<br>Lincoln Borges<br><a href='http://www.lnborges.com.br'>http://www.lnborges.com.br</a>";
-
-
-$mail = new PHPMailer();
-$mail->SetFrom("certificado@lnborges.com.br", "Certificado");
-$mail->Subject    = $subject;
-$mail->MsgHTML(utf8_decode($messageBody));
-$mail->AddAddress($email); 
-$mail->addStringAttachment($pdfdoc, 'certificado.pdf');
-$mail->Send();*/
 
 $certificado="arquivos/$cpf.pdf"; //atribui a variável $certificado com o caminho e o nome do arquivo que será salvo (vai usar o CPF digitado pelo usuário como nome de arquivo)
 $pdf->Output($certificado,'F'); //Salva o certificado no servidor (verifique se a pasta "arquivos" tem a permissão necessária)
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+setlocale( LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.utf-8', 'portuguese' );
+date_default_timezone_set( 'America/Sao_Paulo' );
+?>
 <!DOCTYPE html>
 <html>
 <head>
