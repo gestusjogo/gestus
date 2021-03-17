50cdc2d9d6b6b30bdf46545f2e69fe5f183ecddf
Jonathan Monteiro <jonathanmonteirodev@gmail.com>
2021-03-16 21:23:31 -0300

    removido fpdf

--- certificado/gerador.php
+++ certificado/gerador.php
@@ -1,7 +1,6 @@
 <?php
 setlocale( LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.utf-8', 'portuguese' );
 date_default_timezone_set( 'America/Sao_Paulo' );
-require('fpdf/fpdf.php');
 require('fpdf/alphapdf.php');
 require('PHPMailer/class.phpmailer.php');
 error_reporting(E_ALL);
