eb384587bdcde662ea569b7ea37d61a1fe06b2f6
Jonathan Monteiro <jonathanmonteirodev@gmail.com>
2021-03-16 21:01:24 -0300

    tirado locale

--- certificado/gerador.php
+++ certificado/gerador.php
@@ -1,6 +1,4 @@
 <?php
-setlocale( LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.utf-8', 'portuguese' );
-date_default_timezone_set( 'America/Sao_Paulo' );
 require('fpdf/alphapdf.php');
 require('PHPMailer/class.phpmailer.php');
 
