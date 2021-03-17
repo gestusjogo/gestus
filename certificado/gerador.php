34c82b4a7b0c16cdf497dcd089b9e0887a19ec55
Jonathan Monteiro <jonathanmonteirodev@gmail.com>
2021-03-16 21:27:52 -0300

    gerador

--- certificado/gerador.php
+++ certificado/gerador.php
@@ -29,6 +29,65 @@ $texto3 = '';
 
 $pdf = new AlphaPDF();
 
-echo $nome;
-echo $idade;
+
+// Orientação Landing Page ///
+$pdf->AddPage('L');
+
+$pdf->SetLineWidth(1.5);
+
+
+// desenha a imagem do certificado
+$pdf->Image('certificado.jpg',0,0,297.5);
+
+// opacidade total
+$pdf->SetAlpha(1);
+
+// Mostrar texto no topo
+$pdf->SetFont('Arial', '', 15); // Tipo de fonte e tamanho
+$pdf->SetXY(109,46); //Parte chata onde tem que ficar ajustando a posição X e Y
+$pdf->MultiCell(265, 10, $texto1, '', 'L', 0); // Tamanho width e height e posição
+
+// Mostrar o nome
+$pdf->AddFont('Quattrocento','','Quattrocento-Regular.php');
+$pdf->AddFont('Bukhari Script','','Bukhari Script.php');
+$pdf->SetFont('Bukhari Script', '', 40); // Tipo de fonte e tamanho
+$pdf->SetXY(20,90); //Parte chata onde tem que ficar ajustando a posição X e Y
+$pdf->MultiCell(265, 10, $nome, '', 'C', 0); // Tamanho width e height e posição
+
+// Mostrar o corpo
+$pdf->SetFont('Quattrocento', '', 20); // Tipo de fonte e tamanho
+$pdf->SetXY(10,110); //Parte chata onde tem que ficar ajustando a posição X e Y
+$pdf->MultiCell(0, 10, $texto2, '', 'C', 0); // Tamanho width e height e posição
+
+// Mostrar a data no final
+$pdf->SetFont('Arial', '', 15); // Tipo de fonte e tamanho
+$pdf->SetXY(32,172); //Parte chata onde tem que ficar ajustando a posição X e Y
+$pdf->MultiCell(165, 10, $texto3, '', 'L', 0); // Tamanho width e height e posição
+
+$pdfdoc = $pdf->Output('', 'S');
+
+
+
+// ******** Agora vai enviar o e-mail pro usuário contendo o anexo
+// ******** e também mostrar na tela para caso o e-mail não chegar
+/*
+$subject = 'Seu Certificado do Workshop';
+$messageBody = "Olá $nome<br><br>É com grande prazer que entregamos o seu certificado.<br>Ele está em anexo nesse e-mail.<br><br>Atenciosamente,<br>Lincoln Borges<br><a href='http://www.lnborges.com.br'>http://www.lnborges.com.br</a>";
+
+
+$mail = new PHPMailer();
+$mail->SetFrom("certificado@lnborges.com.br", "Certificado");
+$mail->Subject    = $subject;
+$mail->MsgHTML(utf8_decode($messageBody));
+$mail->AddAddress($email); 
+$mail->addStringAttachment($pdfdoc, 'certificado.pdf');
+$mail->Send();*/
+
+$certificado="arquivos/$cpf.pdf"; //atribui a variável $certificado com o caminho e o nome do arquivo que será salvo (vai usar o CPF digitado pelo usuário como nome de arquivo)
+$pdf->Output($certificado,'F'); //Salva o certificado no servidor (verifique se a pasta "arquivos" tem a permissão necessária)
+// Utilizando esse script provavelmente o certificado ficara salvo em www.seusite.com.br/gerar_certificado/arquivos/999.999.999-99.pdf (o 999 representa o CPF digitado pelo usuário)
+
+$pdf->Output(); // Mostrar o certificado na tela do navegador
+
+
 ?>
