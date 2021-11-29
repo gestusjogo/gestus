e542c277df86cbab7200dc2346cd3daa52dcba94
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-11-29 13:13:41 -0300

    update login background

--- front-end/src/pages/login/index.jsx
+++ front-end/src/pages/login/index.jsx
@@ -1,32 +1,64 @@
 import styled from "styled-components"
-import bg_menu from "../../assets/images/bg_menu.png"
+import bg_casa from "../../assets/images/bg_casa.jpg"
+import jonas from "../../assets/images/jonas.png"
 
 const Login = ()=> {
     return(
-        <Container>
-            <div id="myCanvas" 
+        <>
+            <Backgorund id="myCanvas" 
                 style={{
-                    backgroundImage: `url(${bg_menu})`,
+                    backgroundImage: `url(${bg_casa})`,
                     backgroundPosition: 'center',
                     backgroundSize: 'cover',
                     backgroundRepeat: 'no-repeat' 
-                }}>
-
-            </div>
-        </Container>
+            }}>
+            </Backgorund>
+            <Menu/>
+            <Character src={jonas}/>
+        </>
     )
 }
-const Container = styled.div`
-    #myCanvas{
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        right: 0;
-        left: 0;
-        margin: auto;
-        width: 100%;
-        height: 100%;
-    }
-
+const Backgorund = styled.div`
+    
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    margin: auto;
+    width: 100%;
+    height: 100%;
+    -webkit-filter: blur(15px);
+    -moz-filter: blur(15px);
+    -o-filter: blur(15px);
+    -ms-filter: blur(15px);
+    filter: blur(15px);
+    z-index: -1;
+   
+`
+const Character = styled.img`
+    
+    position: absolute;
+    
+    bottom: 0;
+    left: 0;
+    margin: auto;
+    width: auto;
+    height: 80%;
+    z-index: 10;
+    
+   
+`
+const PlayButton = styled.button`
+    
+`
+const Menu = styled.div`
+    background-color: red;
+    width: 300px;
+    height: 300px;
+    position: absolute;
+    right: 100px;
+    bottom: 100px;
+    z-index: 20;
 `
 export default Login
